428f2ea02734fa422ed0c40c0c2ab9435b1d5437
carlos <csimo0228@gmail.com>
2023-03-26 17:08:41 -0300

    cuarto commit

---
+++ contar.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "loremipsum-1.txt tiene $(wc  -l  lineas)"
+ 
+echo "loremipsum-2.txt tiene $(wc  -l  lineas)" 
+
+echo "loremipsum-3.txt tiene $(wc  -l  lineas)" 
+
+echo "loremipsum-4.txt tiene $(wc  -l  lineas)" 
+ 
+echo "loremipsum-5.txt tiene $(wc  -l  lineas)" 
+
